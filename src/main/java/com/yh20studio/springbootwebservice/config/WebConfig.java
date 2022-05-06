7b7327e572f513df6fc2883767e61556c8605494
yh20studio(tiki) <yh20studio@gmail.com>
2022-05-06 17:45:34 +0900

    fix: cors origins 변경

--- src/main/java/com/yh20studio/springbootwebservice/config/WebConfig.java
+++ src/main/java/com/yh20studio/springbootwebservice/config/WebConfig.java
@@ -11,24 +11,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-	// localHost Web에서 들어오는 접근을 허용하기 위함.
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-			.allowedOrigins("http://127.0.0.1:8000/")
-			.allowedOrigins("https://yh20studio.github.io/flutter_new_calry/")
-			.allowedMethods("POST", "GET", "DELETE", "PUT");
-	}
+    // localHost Web에서 들어오는 접근을 허용하기 위함.
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:8000/")
+                .allowedOrigins("https://yh20studio.github.io/")
+                .allowedMethods("POST", "GET", "DELETE", "PUT");
+    }
 
-	// REST API에 대한 접근 및 리턴이 실행될때 Log 값을 적기 위한 Bean 생성.
-	@Bean
-	public CommonsRequestLoggingFilter requestLoggingFilter() {
-		CommonsRequestLoggingFilter c = new CommonsRequestLoggingFilter();
-		c.setIncludeHeaders(true);
-		c.setIncludeQueryString(true);
-		c.setIncludePayload(true);
-		c.setIncludeClientInfo(true);
-		c.setMaxPayloadLength(1000);
-		return c;
-	}
+    // REST API에 대한 접근 및 리턴이 실행될때 Log 값을 적기 위한 Bean 생성.
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter c = new CommonsRequestLoggingFilter();
+        c.setIncludeHeaders(true);
+        c.setIncludeQueryString(true);
+        c.setIncludePayload(true);
+        c.setIncludeClientInfo(true);
+        c.setMaxPayloadLength(1000);
+        return c;
+    }
 }
